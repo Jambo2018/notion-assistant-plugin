fd32e9de8b8b1b984d72b7b43151d9a3009afa69
Jambo <bxtx1992@gmail.com>
2023-06-25 17:25:01 +0800

    feat: v0.1.0

--- src/link-bookmark.ts
+++ src/link-bookmark.ts
@@ -1,13 +1,12 @@
-
 /**
  * render a card by bookmark-md
  */
 export const generateBookMark = (content: string): HTMLDivElement => {
 	const obj: Record<string, string> = {};
-	(content.split("\n") ?? []).forEach(item => {
-		const [str, key, value = ''] = item.match(/([^:]+):(.*)/)
-		obj[key] = value
-	})
+	(content.split("\n") ?? []).forEach((item) => {
+		const [str, key, value = ""] = item.match(/([^:]+):(.*)/) ?? [];
+		obj[key] = value;
+	});
 	const linkDiv = createDiv();
 	linkDiv.setAttribute("class", "bookmark");
 	linkDiv.onclick = () => {
@@ -15,15 +14,25 @@ export const generateBookMark = (content: string): HTMLDivElement => {
 	};
 	linkDiv.innerHTML = `
 	<div class="bookmark-content">
-	    ${obj.title ? `<div class="bookmark-title">${obj.title}</div>` : ''}
-		${obj.description ? `<div class="bookmark-description">${obj.description}</div>` : ''}
+	    ${obj.title ? `<div class="bookmark-title">${obj.title}</div>` : ""}
+		${
+			obj.description
+				? `<div class="bookmark-description">${obj.description}</div>`
+				: ""
+		}
 		<div class="bookmark-url">
-			${obj.logo ?
-			`<div class="bookmark-url-logo" style="background-image: url('${obj.logo}')" ></div>`
-			: ''} 
+			${
+				obj.logo
+					? `<div class="bookmark-url-logo" style="background-image: url('${obj.logo}')" ></div>`
+					: ""
+			} 
 			<div class="bookmark-url-text">${obj.url}</div>
 		</div>
 	</div>
-	${obj.coverImg ? `<div class="bookmark-cover" style="background-image: url('${obj.coverImg}')" ></div>` : ''} `
+	${
+		obj.coverImg
+			? `<div class="bookmark-cover" style="background-image: url('${obj.coverImg}')" ></div>`
+			: ""
+	} `;
 	return linkDiv;
 };
