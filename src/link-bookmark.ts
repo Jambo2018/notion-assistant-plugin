c62e2d9094b8191b7966581bd6ea0187da89c87d
Jambo <bxtx1992@gmail.com>
2023-09-16 22:11:25 +0800

    fix: rename cls of bookmark

--- src/link-bookmark.ts
+++ src/link-bookmark.ts
@@ -8,30 +8,30 @@ export const generateBookMark = (content: string): HTMLDivElement => {
 		obj[key] = value;
 	});
 	const linkDiv = createDiv();
-	linkDiv.setAttribute("class", "bookmark");
+	linkDiv.setAttribute("class", "ta-bookmark");
 	linkDiv.onclick = () => {
 		window.open(obj.url);
 	};
-	const contentDiv = createDiv({ parent: linkDiv, cls: "bookmark-content" });
+	const contentDiv = createDiv({ parent: linkDiv, cls: "ta-bookmark-content" });
 	if (obj.title) {
-		contentDiv.createDiv({ cls: "bookmark-title", text: obj.title });
+		contentDiv.createDiv({ cls: "ta-bookmark-title", text: obj.title });
 	}
 	if (obj.description) {
 		contentDiv.createDiv({
-			cls: "bookmark-description",
+			cls: "ta-bookmark-description",
 			text: obj.description,
 		});
 	}
-	const urlDiv = contentDiv.createDiv({  cls: "bookmark-url" });
+	const urlDiv = contentDiv.createDiv({  cls: "ta-bookmark-url" });
 	if (obj.logo) {
 		urlDiv.createDiv({
-			cls: "bookmark-url-logo",
+			cls: "ta-bookmark-url-logo",
 			attr: { style: `background-image: url('${obj.logo}')` },
 		});
 	}
-	urlDiv.createSpan({ cls: "bookmark-url-text", text: obj.url });
+	urlDiv.createSpan({ cls: "ta-bookmark-url-text", text: obj.url });
 	if(obj.coverImg){
-		linkDiv.createDiv({cls:"bookmark-cover",attr:{style:`background-image: url('${obj.coverImg}')`}})
+		linkDiv.createDiv({cls:"ta-bookmark-cover",attr:{style:`background-image: url('${obj.coverImg}')`}})
 	}
 	return linkDiv;
 };
